b8953a4612e965d1b55554be26126f43c37e8e70
Lucas Brown <lucasbrown124@gmail.com>
2026-02-09 13:49:47 -0600

    Created prototypes for combat and player scenes
    Doesn't have any of the logic yet, just the nodes and scripts with a few attributes

---
+++ code/combat.gd
@@ -0,0 +1,3 @@
+extends Node
+
+#initalize enemy function
---
+++ scenes/player.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+var character_class = null
+var hp = null
+
+#character class initialization, should be called when run starts
