8b398768f9c30e2ce9523b7765ce0d8c6f560194
Lucas Brown <lucasbrown124@gmail.com>
2026-02-09 14:39:57 -0600

    added placeholder sprite for player and healthbar functionality

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
 var character_class = null
-var hp = null
+var max_hp = 100
+var hp = 50
+
+func _ready() -> void:
+	init_health_bar()
 
 #character class initialization, should be called when run starts
+
+func init_health_bar():
+	$"CharacterDataUI/HealthBar".max_value = max_hp
+	update_health_bar()
+
+func update_health_bar():
+	$"CharacterDataUI/HealthBar".value = hp
