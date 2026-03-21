5be28eb6c0a2cdd3a40bc13e9e7e542dc40f580e
Lucas Brown <123987939+Lunkas-b@users.noreply.github.com>
2026-03-21 16:51:31 -0500

    Added comments

--- code/map.gd
+++ code/map.gd
@@ -1,6 +1,9 @@
 extends Node
 
-var map_grid_column: PackedScene = preload("res://scenes/map_node_container.tscn")
+#this script handles the randomized map generation
+
+#variables and arrays that store the scenes and nodes for path generation
+var map_grid_column: PackedScene = preload("res://scenes/map_grid_column.tscn")
 var map_node: PackedScene = preload("res://scenes/map_node.tscn")
 var map_grid_columns: Array[Control]
 var boss_node
@@ -10,58 +13,88 @@ var column_width
 var column_height
 var column_vertical_offset
 
+#flag that prevents user from moving to a new room before finishing the current encounter
 var room_select_lock = false
 
 func _ready() -> void:
 	randomize()
 	map_texture = $"TextureRect"
+	#sets the dimensions of the columns
 	column_width = (map_texture.size.x-319)/GameManager.MapGridWidth
 	column_height = 704
 	column_vertical_offset = 192
 	
-	init_map_node_containers()
-	init_map_nodes()
+	init_map_grid_columns()
+	init_node_paths()
 	clear_empty_nodes()
 	roll_room_types()
 	generate_boss_node()
-	#print_connected_nodes()
 
-func init_map_node_containers():
+#--------------------------------------
+# This function initializes the columns
+# for the node generation grid
+#--------------------------------------
+func init_map_grid_columns():
 	for i in range(GameManager.MapGridWidth):
 		var new_column = map_grid_column.instantiate()
 		new_column.column_index = i
+		#adds the new column to the map_grid_columns array
 		map_grid_columns.append(new_column)
 		add_child(new_column)
+		#applies the dimensions and position for the column
 		new_column.init_container_transform()
+		#generates the empty nodes for the full column
 		new_column.init_empty_nodes()
 
-func init_map_nodes():
+#----------------------------------------------------------------
+# This function randomly generates the paths for the current run. 
+# Generates the paths one at a time, ensuring all map nodes are 
+# within the grid and the paths do not cross
+#----------------------------------------------------------------
+func init_node_paths():
+	#creates 6 paths
 	for i in range(6):
+		#generates a random starting position for the current path
 		var start_pos = randi() % GameManager.MapGridHeight
+		#makes sure that the starting point for the first and second path are not the same, ensuring atleast two starting node options
 		if i == 1 and map_grid_columns[0].existing_node_flags[start_pos] == 1:
+			#randomly generates a new starting point until it is not the same as the first path starting point
 			while map_grid_columns[0].existing_node_flags[start_pos] != 1:
 				start_pos = randi() % GameManager.MapGridHeight
+		#sets the current node to the starting position
 		var current_node = map_grid_columns[0].map_nodes[start_pos]
 		current_node.is_empty = false
+		#for every column up to the boss node
 		for j in range(1,GameManager.MapGridWidth):
+			#generates the next position of the path based on the 3 closest nodes in the next column
 			var next_position = randi() % 3 + (current_node.row_index-1)
+			#makes sure that paths cant cross
 			while(map_grid_columns[j-1].check_for_crossing(current_node.row_index, next_position)):
 				next_position = randi() % 3 + (current_node.row_index-1)
+			#ensures that the next position is not outside of the map grid
 			next_position = clamp(next_position, 0, GameManager.MapGridHeight-1)
 			
+			#gets the next node based on the generated next position
 			var next_node = map_grid_columns[j].map_nodes[next_position]
+			#sets the flag for the next node to 1
 			map_grid_columns[j].existing_node_flags[next_position] = 1
+			#prevents duplicate nodes in the forward connecting array for the current node
 			if not next_node in current_node.forward_connected_nodes:
 				current_node.forward_connected_nodes.append(next_node)
-			
+			#renders the line between the current and next node
 			current_node.create_path_line(next_node)
-			
+			#stores the positions of the current and next nodes to the connections array, used in cross prevention
 			map_grid_columns[j-1].connections.append(Vector2(current_node.row_index, next_position))
 			
+			#adds the current node to the next node backwards connection array
 			next_node.backward_connected_nodes.append(current_node)
 			next_node.is_empty = false
 			current_node = next_node
 
+#-------------------------------------------------
+# This function rolls the room types for each node
+# using weights and specific generation rules
+#-------------------------------------------------
 func roll_room_types():
 	var room_weights = {
 			GameManager.RoomTypes.Combat: 50,
@@ -71,51 +104,77 @@ func roll_room_types():
 		}
 	for column in map_grid_columns:
 		for node in column.map_nodes:
+			#gets all the nodes in the paths leading to the current node
 			var previous_nodes = node.get_prev_nodes()
 			#print_previous_nodes(node, previous_nodes)
 			
+			#prevents nodes having their rooms rerolled multiple times
 			if not node.room_type_rolled:
+				#if not a starting node
 				if not node.get_parent().column_index == 0:
+					#creates a duplicate of the weights dictionary so that they can be adjusted for this specific node
 					var weights = room_weights.duplicate()
 					adjust_weights(node, previous_nodes, weights)
-					print_previous_nodes(node, previous_nodes)
-					print("weights:")
-					for weight in weights:
-						print(str(weight) + ": " + str(weights[weight]))
+					#print_previous_nodes(node, previous_nodes)
+					#print("weights:")
+					#for weight in weights:
+						#print(str(weight) + ": " + str(weights[weight]))]
 					node.room_type = roll_room(weights)
 				node.room_type_rolled = true
 				node.update_sprite()
 
+#------------------------------------------------------------
+# This function adjusts the room weights for the current node
+# ensuring that duplicate combat and event nodes are less 
+# likely and that there can only be one rest every few nodes
+# and one shop every few nodes
+#------------------------------------------------------------
 func adjust_weights(node, previous_nodes, weights):
 	if not previous_nodes.is_empty():
-		for weight in weights:
-			if type_in_last_n_rooms(node, previous_nodes, weight, 1):
-				weights[weight] *= 0.3
+		if type_in_last_n_rooms(node, previous_nodes, GameManager.RoomTypes.Combat, 1):
+			weights[GameManager.RoomTypes.Combat] *= 0.3
+		if type_in_last_n_rooms(node, previous_nodes, GameManager.RoomTypes.Event, 1):
+			weights[GameManager.RoomTypes.Event] *= 0.3
 		if type_in_last_n_rooms(node, previous_nodes, GameManager.RoomTypes.Shop, 3):
 			weights[GameManager.RoomTypes.Shop] = 0
 		if type_in_last_n_rooms(node, previous_nodes, GameManager.RoomTypes.Rest, 3):
 			weights[GameManager.RoomTypes.Rest] = 0
 
+#-----------------------------------------------
+# This function checks if there is a node with a
+# specific room type within the last n nodes
+#-----------------------------------------------
 func type_in_last_n_rooms(node, previous_nodes, type:GameManager.RoomTypes, n):
 	var index = 0
-	while index < previous_nodes.size() and node.get_parent().column_index-previous_nodes[index].get_parent().column_index <= n:
+	while index < previous_nodes.size() and node.get_parent().column_index - previous_nodes[index].get_parent().column_index <= n:
 		if previous_nodes[index].room_type == type:
 			return true
 		index += 1
 	return false
 
+#-------------------------------------------------
+# This function rolls for a specific room based on
+# the provided weights
+#-------------------------------------------------
 func roll_room(weights):
 	var total = 0
+	#gets the total of all the weights
 	for value in weights.values():
 		total += value
 	
+	#rolls a random integer between 0 and the total-1
 	var roll = randi_range(0, total-1)
 	var cumulative = 0
+	#adds the weight of each room in descending order until the cumulative value is greater than the roll number
 	for key in weights.keys():
 		cumulative += weights[key]
 		if roll < cumulative:
 			return key
 
+#----------------------------------------------------------
+# This function clears all the nodes that are not connected 
+# through any of the generated paths
+#----------------------------------------------------------
 func clear_empty_nodes():
 	for column in map_grid_columns:
 		for i in range(column.map_nodes.size()-1, -1, -1):
@@ -133,9 +192,9 @@ func generate_boss_node():
 		node.create_path_line(boss_node)
 
 #------------------ Debug Functions ------------------
-func print_container_data():
-	for container in map_grid_columns:
-		container.print_transform_data()
+func print_column_data():
+	for column in map_grid_columns:
+		column.print_transform_data()
 
 func print_connected_nodes():
 	print("map_grid_columns.size(): " + str(map_grid_columns.size()))
--- code/map_node.gd
+++ code/map_node.gd
@@ -11,17 +11,18 @@ var forward_connected_nodes: Array[Control]
 var backward_connected_nodes: Array[Control]
 var connecting_lines: Array[Line2D]
 
+#gives spacing between the connecting lines and their nodes 
 var line_offset_radius = 48
 
 func _ready() -> void:
-	forward_connected_nodes.clear()
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 
 func update_position():
 	var column = get_parent()
 	global_position = Vector2(column.custom_minimum_size.x*column.column_index+column.get_parent().column_width/2-size.x/2, column.get_parent().column_vertical_offset+column.get_parent().column_height/GameManager.MapGridHeight * row_index+size.y/2)
-	
+
+#changes the sprites to correlate to their room type
 func update_sprite():
 	match room_type:
 		GameManager.RoomTypes.Combat:
@@ -38,15 +39,19 @@ func update_sprite():
 			hoverTexture = preload("res://sprites/map/shop_icon_hovering.png")
 	$"NodeTexture".texture = idleTexture
 
+#renders the line between connected nodes
 func create_path_line(next_node):
 	var new_line = Line2D.new()
 	new_line.default_color = "#75503f"
 	new_line.begin_cap_mode = Line2D.LINE_CAP_ROUND
 	new_line.end_cap_mode = Line2D.LINE_CAP_ROUND
 	new_line.width = 6
+	#line starting point
 	var p1 = Vector2($"NodeTexture".size.x/2, $"NodeTexture".size.y/2)
+	#line end point
 	var p2 = Vector2(next_node.global_position.x-global_position.x+$"NodeTexture".size.x/2, next_node.global_position.y-global_position.y+abs($"NodeTexture".size.y/2))
 	var direction = (p2-p1).normalized()
+	#offsets the start and endpoints of the line so that they are not directly next to the nodes
 	p1 += direction * line_offset_radius
 	p2 -= direction * line_offset_radius
 	new_line.add_point(p1)
@@ -54,22 +59,41 @@ func create_path_line(next_node):
 	add_child(new_line)
 	connecting_lines.append(new_line)
 
+#returns all the nodes in every path leading to this node
 func get_prev_nodes() -> Array[Control]:
+	#the array that will be returned
 	var prev_path: Array[Control]
+	#queue that traverses the paths
 	var queue = backward_connected_nodes.duplicate()
 	
 	while not queue.is_empty():
 		var current_node = queue.front()
 		#print_queue(queue, prev_path)
+		#gets all the nodes connecting to the back of the node at the front of the queue
 		for node in current_node.backward_connected_nodes:
+			#prevents duplicate nodes in the queue
 			if not node in queue:
 				queue.append(node)
+		#prevents duplicate nodes in the resulting array
 		if not current_node in prev_path:
 			prev_path.append(current_node)
+		#removes the node at the front of the queue
 		queue.remove_at(0)
 	#print_queue(queue, prev_path)
 	return prev_path
+#----------------------------Input Handling Functions--------------------------------------
 
+#changes the texture when the mouse cursor is over the node
+func _on_mouse_entered():
+	if $"NodeTexture".texture != hoverTexture:
+		$"NodeTexture".texture = hoverTexture
+
+#changes the texture once the mouse is no longer hovering over the node
+func _on_mouse_exited():
+	if $"NodeTexture".texture != idleTexture:
+		$"NodeTexture".texture = idleTexture
+
+#--------------------------------Debug Functions-------------------------------------------
 func print_queue(queue, prev_path):
 	print("Queue:")
 	for node in queue:
@@ -87,11 +111,3 @@ func print_backward_connected_nodes():
 	print("Backward connected nodes:")
 	for node in backward_connected_nodes:
 		node.print_grid_position()
-
-func _on_mouse_entered():
-	if $"NodeTexture".texture != hoverTexture:
-		$"NodeTexture".texture = hoverTexture
-
-func _on_mouse_exited():
-	if $"NodeTexture".texture != idleTexture:
-		$"NodeTexture".texture = idleTexture
