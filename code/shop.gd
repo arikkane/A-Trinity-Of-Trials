1f71a865638d0d761e3c327ba12b9cd8f01efb0e
Lucas Brown <lucasbrown124@gmail.com>
2026-04-06 09:25:06 -0500

    started shop implementation

--- code/shop.gd
+++ code/shop.gd
@@ -1,13 +1,20 @@
 extends Node
 
 var room_data
+var cards: Array[Control]
 
 func _ready():
 	$"NextRoomButton".pressed.connect(_on_button_pressed)
+	cards = init_card_data()
 
 func init_room_data(data: ShopData):
 	room_data = data
 	print_room_data()
+
+func init_card_data():
+	var card_pool: Array[int] = {}
+	for card in GameManager.Deck.card_data["cards"][GameManager.PlayerClass]:
+		card_pool.append(card.get("id"));
 
 func print_room_data():
 	print("ID: " + room_data.id)
