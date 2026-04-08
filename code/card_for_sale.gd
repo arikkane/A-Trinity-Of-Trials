33d60ea9bd1a3138feff64e67ab7420cba066866
Lucas Brown <123987939+Lunkas-b@users.noreply.github.com>
2026-04-07 19:51:17 -0500

    cards display properly in shop

--- code/card_for_sale.gd
+++ code/card_for_sale.gd
@@ -4,15 +4,19 @@ var card_data: CardData
 var card_price: int
 var can_afford: bool
 
-func is_affordable():
-	can_afford = GameManager.PlayerGold >= card_price
-	if can_afford:
-		add_theme_color_override("default", "#00a300")
-	else
-		
+func check_if_affordable():
+	if GameManager.PlayerGold >= card_price:
+		$"PricetagTexture/PriceText".add_theme_color_override("default_color", "#00a300")
+		can_afford = true
+	else:
+		$"PricetagTexture/PriceText".add_theme_color_override("default_color", "dd2400")
+		can_afford = false
 
 func generate_price(base_price):
 	card_price = randi_range(base_price-10, base_price+10)
 
 func update_labels():
-	
+	$"CardTexture/Type".add_text(card_data.type)
+	$"CardTexture/Description".add_text(card_data.description)
+	$"PricetagTexture/PriceText".add_text(str(card_price))
+	check_if_affordable()
