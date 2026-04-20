05004a86f5d928924666dcee4bddc969bbcaf5c3
christian Cormier <ccpokemon@yahoo.com>
2026-04-20 11:15:08 -0500

    fixed main menu UI and added title and button icons
    added icons and fixed UI

--- code/main_menu.gd
+++ code/main_menu.gd
@@ -6,16 +6,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GameManager.UIOverlay.hide_ui()
 	AudioManager.play_music_track("main_menu")
+	GameManager.UIOverlay.hide_ui()
 	setup_menu_ui()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-	
 func setup_menu_ui():
 	var menu_container = VBoxContainer.new()
 	add_child(menu_container)
@@ -23,28 +17,35 @@ func setup_menu_ui():
 	menu_container.alignment = BoxContainer.ALIGNMENT_CENTER
 	menu_container.add_theme_constant_override("separation", 15)
 
-	var play_button = _create_button("Play Game", _on_play_button_pressed)
-	var settings_button = _create_button("Settings", _on_settings_button_pressed)
-	var exit_button = _create_button("Exit", _on_exit_button_pressed)
+	var play_button = create_button("res://assets/UI&Charactors/new_game_icon.png",_on_play_button_pressed)
+	var settings_button = create_button("res://assets/UI&Charactors/settings_con.png",_on_settings_button_pressed)
+	var exit_button = create_button("res://assets/UI&Charactors/exit_icon.png",_on_exit_button_pressed)
 
 	menu_container.add_child(play_button)
 	menu_container.add_child(settings_button)
 	menu_container.add_child(exit_button)
 
 	play_button.grab_focus()
+	
 
+#function that switches to the initial combat scene of the game
 func _on_play_button_pressed():
-	SceneManager.change_scene("res://scenes/ClassSelection.tscn")
-
+	SceneManager.change_scene(game_scene_path)
+	
+#fucntion that switches to the settings scene
 func _on_settings_button_pressed():
-	SceneManager.change_scene("res://scenes/SettingsMenu.tscn")
-
+	SceneManager.change_scene(settings_scene_path)
+	
+#function that closes the program
 func _on_exit_button_pressed():
 	get_tree().quit()
 
-func _create_button(text: String, callback: Callable) -> Button:
+func create_button(icon: String,callback : Callable) -> Button:
 	var button = Button.new()
-	button.text = text
-	button.custom_minimum_size = Vector2(200,50)
+	button.custom_minimum_size = Vector2(200, 50)
+	button.icon = load(icon)
+	button.expand_icon = true
+	button.icon_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	button.vertical_icon_alignment = VERTICAL_ALIGNMENT_CENTER
 	button.pressed.connect(callback)
 	return button
